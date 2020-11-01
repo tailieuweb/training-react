43bccbe69fd8cec03a671c717beb493d03bedef3
lethanhhung275 <33917744+lethanhhung275@users.noreply.github.com>
2020-11-01 22:09:57 +0700

    Demo Group I

---
+++ src/mockdata/Mockdata.js
@@ -0,0 +1,36 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const Mockdata = [
+  {
+    id: uuidv4(),
+    name: "Đêm trong những khu sơ tán tránh bão Molave",
+    level: 2 // high
+  },
+  {
+    id: uuidv4(),
+    name: "Lo sợ trước ‘cuồng phong’ Molave",
+    level: 0 // low
+  },
+  {
+    id: uuidv4(),
+    name: "Sống chung với rác",
+    level: 1 // medium
+  },
+  {
+    id: uuidv4(),
+    name: "Cuộc chiến thương mại của Trump vẫn chưa giúp Mỹ hưởng lợi",
+    level: 0 // low
+  },
+  {
+    id: uuidv4(),
+    name: "Fiingroup: Lợi nhuận doanh nghiệp đã phục hồi theo mô hình chữ V",
+    level: 2 // high
+  },
+  {
+    id: uuidv4(),
+    name: "Ronaldo lỡ hẹn với Messi vì nCoV",
+    level: 1 // medium
+  }
+];
+
+export default Mockdata;
