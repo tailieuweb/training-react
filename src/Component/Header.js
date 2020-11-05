a9c6b919cc278aefb3db457aae917460602e5073
quockhanhcode <01673khanh@gmail.com>
2020-11-05 17:03:27 +0700

    quockhanh

---
+++ src/Component/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="jumbotron text-center">
+        <h1 className="display-3">Project Quản Lý Sinh Viên</h1>
+        <p className="lead">Với Dữ Liệu Json</p>
+      </div>
+        );
+    }
+}
+
+export default Header;
