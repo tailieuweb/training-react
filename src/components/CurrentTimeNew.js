c376744e91d1e3509370b85b3eb6170bf56bd8db
hoangtularva <hoangtukg295@gmail.com>
2020-11-06 15:33:41 +0700

    Change CurrentTimeNew and note list

--- src/components/CurrentTimeNew.js
+++ src/components/CurrentTimeNew.js
@@ -1,10 +1,13 @@
+//Hiển thị thời gian hiện tại
 import React from "react";
 
 const CurrentTimeNew = () => {
+  //Sử dụng hook tạo biến chứa giá trị thời gian
   const [time, setTime] = React.useState(new Date());
   const [day, setDay] = React.useState(new Date().getDate());
   const [month, setMonth] = React.useState(new Date().getMonth() + 1);
   const [year, setYear] = React.useState(new Date().getFullYear());
+  // Sau thời gian là 1s thì sẽ tự động cập nhật giá trị mới vào các biến
   setInterval(() => {
     setTime(new Date());
     setDay(new Date().getDate());
@@ -13,7 +16,22 @@ const CurrentTimeNew = () => {
   }, 1000);
   return (
     <React.Fragment>
-      <p>{time.toLocaleTimeString()}, ngày {day}, tháng {month}, năm {year}</p>
+      <div className="current-time">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-center">
+              {/* Thời gian hiện tại */}
+              <p className="time">{time.toLocaleTimeString()}</p>
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-center">
+              <p className="date">
+                {/* Ngày tháng năm */}
+                Ngày {day}, tháng {month}, năm {year}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
