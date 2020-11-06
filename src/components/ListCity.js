b6689f1d96e712d1fa94284e660cc4ae0cb4baa9
hoangtularva <hoangtukg295@gmail.com>
2020-11-06 10:22:19 +0700

    change list city and list key

--- src/components/ListCity.js
+++ src/components/ListCity.js
@@ -1,5 +1,86 @@
+// Danh sách các thành phố lớn ở Việt Nam, sử dụng để lấy thông tin thời tiết cho phép người dùng có thể xem nhanh thông tin thời tiết ở nơi gần nhất
+
 const ListCity = [
- 
+  {
+    id: 1559969,
+    name: "Tỉnh Nghệ An",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 104.833328,
+      lat: 19.33333,
+    },
+  },
+  {
+    id: 1559970,
+    name: "Tỉnh Ninh Bình",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 105.833328,
+      lat: 20.25,
+    },
+  },
+  {
+    id: 1559971,
+    name: "Tỉnh Ninh Thuận",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 108.833328,
+      lat: 11.75,
+    },
+  },
+  {
+    id: 1559972,
+    name: "Tỉnh Sóc Trăng",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 105.833328,
+      lat: 9.66667,
+    },
+  },
+  {
+    id: 1559975,
+    name: "Tỉnh Trà Vinh",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 106.25,
+      lat: 9.83333,
+    },
+  },
+  {
+    id: 1559976,
+    name: "Tỉnh Tuyên Quang",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 105.25,
+      lat: 22.116671,
+    },
+  },
+  {
+    id: 1559977,
+    name: "Tỉnh Vĩnh Long",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 106.0,
+      lat: 10.16667,
+    },
+  },
+  {
+    id: 1560349,
+    name: "Yen Bai",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 104.866669,
+      lat: 21.700001,
+    },
+  },
   {
     id: 1562414,
     name: "Vung Tau",
@@ -10,7 +91,6 @@ const ListCity = [
       lat: 10.34599,
     },
   },
- 
   {
     id: 1562798,
     name: "Vinh",
@@ -21,18 +101,6 @@ const ListCity = [
       lat: 18.66667,
     },
   },
- 
-  {
-    id: 1565022,
-    name: "Thu Dau Mot",
-    state: "",
-    country: "VN",
-    coord: {
-      lon: 106.650002,
-      lat: 10.96667,
-    },
-  },
- 
   {
     id: 1566083,
     name: "Thanh pho Ho Chi Minh",
@@ -43,7 +111,6 @@ const ListCity = [
       lat: 10.75,
     },
   },
- 
   {
     id: 1566166,
     name: "Thanh Hoa",
@@ -54,7 +121,6 @@ const ListCity = [
       lat: 19.799999,
     },
   },
-
   {
     id: 1566319,
     name: "Thai Nguyen",
@@ -65,29 +131,16 @@ const ListCity = [
       lat: 21.592779,
     },
   },
-  
   {
-    id: 1568510,
-    name: "Rach Gia",
+    id: 1569805,
+    name: "Tỉnh Phú Yên",
     state: "",
     country: "VN",
     coord: {
-      lon: 105.083328,
-      lat: 10.01667,
+      lon: 109.166672,
+      lat: 13.16667,
     },
   },
-
-  {
-    id: 1573517,
-    name: "Nam Dinh",
-    state: "",
-    country: "VN",
-    coord: {
-      lon: 106.166672,
-      lat: 20.41667
-    }
-  },
- 
   {
     id: 1571058,
     name: "Phan Thiet",
@@ -98,29 +151,47 @@ const ListCity = [
       lat: 10.93333,
     },
   },
-  
   {
-    id: 1573517,
-    name: "Nam Dinh",
+    id: 1572151,
+    name: "Nha Trang",
     state: "",
     country: "VN",
     coord: {
-      lon: 106.166672,
-      lat: 20.41667,
+      lon: 109.183327,
+      lat: 12.25,
     },
   },
 
   {
-    id: 1575627,
-    name: "Long Xuyen",
+    id: 1572594,
+    name: "Tỉnh Hòa Bình",
     state: "",
     country: "VN",
     coord: {
-      lon: 105.416672,
-      lat: 10.38333,
+      lon: 105.25,
+      lat: 20.33333,
     },
   },
-
+  {
+    id: 1577882,
+    name: "Tỉnh Lâm Ðồng",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 108.333328,
+      lat: 11.5,
+    },
+  },
+  {
+    id: 1579634,
+    name: "Tỉnh Khánh Hòa",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 109.0,
+      lat: 12.33333,
+    },
+  },
   {
     id: 1580240,
     name: "Hue",
@@ -131,18 +202,26 @@ const ListCity = [
       lat: 16.466669,
     },
   },
-
   {
-    id: 1580410,
-    name: "Ha Long",
+    id: 1580578,
+    name: "Thành phố Hồ Chí Minh",
     state: "",
     country: "VN",
     coord: {
-      lon: 107.080002,
-      lat: 20.951111,
+      lon: 106.666672,
+      lat: 10.83333,
     },
   },
-  
+  {
+    id: 1581047,
+    name: "Ha Tinh",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 105.900002,
+      lat: 18.33333,
+    },
+  },
   {
     id: 1581130,
     name: "Ha Noi",
@@ -153,18 +232,47 @@ const ListCity = [
       lat: 21.0245,
     },
   },
- 
   {
-    id: 1581297,
-    name: "Thành Phố Hải Phòng",
+    id: 1581298,
+    name: "Haiphong",
     state: "",
     country: "VN",
     coord: {
-      lon: 106.583328,
-      lat: 20.83333,
+      lon: 106.68222,
+      lat: 20.85611,
     },
   },
- 
+  {
+    id: 1582720,
+    name: "Tỉnh Ðồng Nai",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 107.166672,
+      lat: 11.0,
+    },
+  },
+
+  {
+    id: 1584169,
+    name: "Tỉnh Ðắc Lắk",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 108.166672,
+      lat: 12.83333,
+    },
+  },
+  {
+    id: 1584534,
+    name: "Tỉnh Bà Rịa-Vũng Tàu",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 107.25,
+      lat: 10.58333,
+    },
+  },
   {
     id: 1586203,
     name: "Can Tho",
@@ -175,27 +283,35 @@ const ListCity = [
       lat: 10.03333,
     },
   },
- 
   {
-    id: 1587923,
-    name: "Bien Hoa",
+    id: 1586443,
+    name: "Ca Mau",
     state: "",
     country: "VN",
     coord: {
-      lon: 106.816673,
-      lat: 10.95,
+      lon: 105.150002,
+      lat: 9.17694,
     },
   },
-
   {
-    id: 1572151,
-    name: "Nha Trang",
+    id: 1586896,
+    name: "Buon Ma Thuot",
     state: "",
     country: "VN",
     coord: {
-      lon: 109.183327,
-      lat: 12.25
-    }
+      lon: 108.050003,
+      lat: 12.66667,
+    },
+  },
+  {
+    id: 1591474,
+    name: "Thanh pho Bac Lieu",
+    state: "",
+    country: "VN",
+    coord: {
+      lon: 105.724442,
+      lat: 9.285,
+    },
   },
 ];
 export default ListCity;
--- src/components/ListKey.js
+++ src/components/ListKey.js
@@ -1,2 +1,3 @@
+// Giá trị key API sử dụng cho request từ openweather
 const APIkey = "045f6ed3428f74c636319cc9725b7bc6";
 export default APIkey;
