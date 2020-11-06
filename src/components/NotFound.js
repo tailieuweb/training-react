9f09ef3676cc25f02dbe762d4fe6e23dbecebb61
MaximusNguyen15 <nguyenvinhhientv1590@gmail.com>
2020-11-06 15:18:53 +0700

    small update NotFound file

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,47 +1,16 @@
+// Khi kết quả tìm kiếm, hoặc có lỗi sảy ra thì component sẽ được gọi tại App.js
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFrown } from '@fortawesome/free-solid-svg-icons';
-import styled from 'styled-components';
-import ResultFadeIn from './ResultFadeIn';
-
-const NotFoundWrapper = styled.div`
-  max-width: 600px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 100px auto 0;
-  padding: 20px;
-  opacity: 0;
-  visibility: hidden;
-  position: relative;
-  border-radius: 10px;
-  top: 20px;
-  background-color: rgba(255, 255, 255, 0.3);
-  border-radius: 10px;
-  animation: ${ResultFadeIn} 0.5s 1.4s forwards;
-`;
-
-const NotfoundIcon = styled.span`
-  display: block;
-  text-align: center;
-  color: #ffffff;
-  font-size: 40px;
-  margin-right: 10px;
-`;
-
-const NotFoundText = styled.span`
-  color: #ffffff;
-  font-size: 17px;
-`;
-
+import {faSadTear} from '@fortawesome/free-solid-svg-icons';
+import '../scss/Style.scss'
 const NotFound = () => {
   return (
-    <NotFoundWrapper>
-      <NotfoundIcon>
-        <FontAwesomeIcon icon={faFrown} />
-      </NotfoundIcon>
-      <NotFoundText>Có lỗi xảy ra!</NotFoundText>
-    </NotFoundWrapper>
+    <div className="no-result">
+      <span className="no-result-icon">
+        <FontAwesomeIcon icon={faSadTear} />
+      </span>
+      <span class="no-result-text">Có lỗi xảy ra!</span>
+    </div>
   );
 };
 
