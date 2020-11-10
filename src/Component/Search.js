45f7b85425f8bfea016c45d7beaecfa435b400e8
tranconghoaxx <tranhoa300696@gmail.com>
2020-11-10 14:15:44 +0700

    demo project nhom b

---
+++ src/Component/Search.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import EditUser from './EditUser';
+
+class Search extends Component {
+
+  constructor(props) {
+    super(props);
+    //Tạo 1 biến trung gian
+    this.state ={
+      tempValue: '',
+      userObj:{}
+    }
+    
+  }
+
+  isChange = (event) =>{
+    console.log(event.target.value)
+    this.setState({
+      tempValue: event.target.value
+    })
+    this.props.GiveDatas(this.state.tempValue)
+   //this.props.GiveDatas(event.target.value);
+  }
+
+  //Lấy dữ liệu gửi lên App
+  getEditUser = (info) =>{
+    this.setState({
+      userObj: info
+    });
+    this.props.getEditUserApp(info)
+  }
+
+  isShowEditUser = () =>
+  {
+    if(this.props.EditUser === true)
+    {
+      return <EditUser
+      getEditUser = {(info)=>this.getEditUser(info)}
+      userEditObject ={this.props.userEditObject}
+      changeEditUserStatus={this.props.changeEditUserStatus()}/>
+    }
+  }
+render    () {
+        return (
+            <div>
+            <div className="col-12">
+            <div className="row">
+              {this.isShowEditUser()}
+            </div>
+          </div>
+          <div className="col-12">
+              <div className="form-group">
+                <div className="btn-group">
+                  <input type="text" className="form-control" onChange={(event)=>this.isChange(event)} placeholder="Nhập thông tim cần tìm..." />
+                  <button type="button" className="btn btn-success" onClick={(dulieu)=>this.props.GiveDatas(this.state.tempValue)}>Search</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-12">
+              <hr />
+            </div>
+          </div>
+    
+        );
+    }
+}
+
+export default Search;
